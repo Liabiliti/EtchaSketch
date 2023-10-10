b2a2723d90d1c7dac365e9d8436d57949eef1830
NP <nicholas_b_01@hotmail.com>
2023-10-10 15:07:02 +1100

    Popup completed

--- functions.js
+++ functions.js
@@ -4,15 +4,20 @@ const value = document.querySelector(".gridValue");
 const popupButton = document.querySelector(".popup");
 const submit = document.querySelector(".submit");
 
-popupButton.addEventListener("click", function()
-{
+
+popupButton.addEventListener("click", function () {
     var popup = document.querySelector(".popupContainer");
     popup.classList.toggle("show");
     grandparentContainer.classList.toggle("show");
+    if (popup.classList.contains("show")) {
+        popupButton.textContent = "Keep Grid";
+    }
+    else {
+        popupButton.textContent = "New Grid";
+    }
 })
 
-slider.addEventListener("click", function()
-{
+slider.addEventListener("click", function () {
     value.textContent = slider.value;
 })
 
@@ -36,19 +41,27 @@ function getGrid(squares) {
         }
         grandparentContainer.append(parentContainer);
     }
+    const grids = document.querySelectorAll(".grid");
+
+    grids.forEach((grid) => {
+        grid.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "black";
+            console.log("Mouse overing");
+        });
+        grid.addEventListener("mouseout", (event) => {
+            event.target.style.backgroundColor = "black";
+            console.log("Mouse outing");
+        });
+    });
 }
 
 getGrid(squares);
 
-const grids = document.querySelectorAll(".grid");
-
-grids.forEach((grid) => {
-    grid.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "black";
-        console.log("Mouse overing");
-    });
-    grid.addEventListener("mouseout", (event) => {
-        event.target.style.backgroundColor = "black";
-        console.log("Mouse outing");
-    });
-});
+submit.addEventListener("click", function () {
+    console.log(slider.value);
+    while (grandparentContainer.firstChild) {
+        grandparentContainer.removeChild(grandparentContainer.firstChild);
+    }
+    getGrid(slider.value);
+    popupButton.click();
+})
